66516f1fa7f0ef2fe7b2b8be15cb7e0aa85c49ab
Vedansh111 <123576150+Vedansh111@users.noreply.github.com>
2024-06-14 14:23:53 +0530

    Adding the navbar

---
+++ src/pages/LandingPage/StickyNavbar.jsx
@@ -0,0 +1,145 @@
+import { useEffect, useState } from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+import icons_slime from "../../assets/images/icons_slime.png";
+import { Link, useNavigate } from "react-router-dom";
+
+export function StickyNavbar() {
+  const [openNav, setOpenNav] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="#" className="flex items-center text-base">
+          Home
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="#" className="flex items-center text-base">
+          About Us
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="#" className="flex items-center text-base">
+          Weight Loss Treatment
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="#" className="flex items-center text-base">
+          Hair & Beauty Treatment
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <Link to="#" className="flex items-center text-base">
+          Contact Us
+        </Link>
+      </Typography>
+    </ul>
+  );
+
+  return (
+    <Navbar className="absolute top-0 z-10 h-max max-w-full rounded-none px-2 py-1.5 lg:px-8 lg:py-2.5">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <img src={icons_slime} />
+        <div className="flex items-center gap-4">
+          <div className="mr-4 hidden lg:block">{navList}</div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              onClick={() => {
+                navigate("/login");
+              }}
+              variant="gradient"
+              size="sm"
+              className="hidden lg:inline-block"
+            >
+              <span className="text-sm">Sign in</span>
+            </Button>
+          </div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
+        </div>
+      </div>
+      <MobileNav open={openNav}>
+        {navList}
+        <div className="flex items-center gap-x-1">
+          <Button fullWidth variant="gradient" size="sm">
+            <span className="text-sm">Sign in</span>
+          </Button>
+        </div>
+      </MobileNav>
+    </Navbar>
+  );
+}
