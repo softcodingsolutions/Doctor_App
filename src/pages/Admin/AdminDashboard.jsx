f883a3d77994fe7cf1a3c1ef0decccebb9c8a848
Vedansh111 <123576150+Vedansh111@users.noreply.github.com>
2024-06-21 16:12:56 +0530

    changes on the admin dashboard

--- src/pages/Admin/AdminDashboard.jsx
+++ src/pages/Admin/AdminDashboard.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import ThComponent from "../../components/ThComponent";
+import TdComponent from "../../components/TdComponent";
 
 function AdminDashboard() {
+  const [getConsultingTime, setGetConsultingTime] = useState([]);
+  const [getMachineTime, setGetMachineTime] = useState([]);
   const context = useOutletContext();
 
   return (
@@ -8,18 +13,14 @@ function AdminDashboard() {
       <div className="w-full h-screen hidden sm:block sm:w-20 xl:w-60 flex-shrink-0">
         .
       </div>
-      <div className=" h-screen flex-grow overflow-x-hidden overflow-auto flex flex-wrap content-start p-2">
-        <div className="w-full sm:flex p-2 items-end">
-          <div className="sm:flex-grow flex justify-between">
-            <div className="">
-              <div className="flex items-center">
-                <div className="text-3xl font-bold">Hello Admin!</div>
-              </div>
-            </div>
+      <div className="h-screen flex-grow overflow-auto flex flex-wrap content-start p-1">
+        <div className="w-full h-full p-2 flex flex-col gap-1">
+          <div className="sm:flex-grow flex justify-between overflow-x-hidden gap-1">
+            <div> </div>
             <button
               onClick={context[0]}
               type="button"
-              className="block sm:hidden hover:scale-110"
+              className="block sm:hidden"
             >
               <img
                 src={`https://assets.codepen.io/3685267/res-react-dash-sidebar-open.svg`}
@@ -27,6 +28,112 @@ function AdminDashboard() {
                 className="w-full h-full"
               />
             </button>
+          </div>
+          <div className="flex w-full h-full items-center justify-center flex-col gap-1">
+            {/* consulting time table */}
+            <div className="flex w-full flex-col items-center p-1 h-1/2">
+              <div className="text-2xl font-semibold tracking-wide">
+                Consulting Time Slot
+              </div>
+              <div className="animate-fade-left animate-delay-75 w-full shadow-gray-400 shadow-inner border rounded-md border-gray-100 animate-once animate-ease-out overflow-auto h-[93%]">
+                <table className="w-full min-w-[460px] z-0">
+                  <thead className="uppercase">
+                    <tr className="bg-[#1F2937] text-white rounded-md">
+                      <ThComponent
+                        moreClasses={"rounded-tl-md rounded-bl-md"}
+                        name="No."
+                      />
+                      <ThComponent name="Doctor Name" />
+                      <ThComponent name="Slot" />
+                      <ThComponent
+                        name="Time"
+                        moreClasses={"rounded-tr-md rounded-br-md"}
+                      />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {getConsultingTime.length === 0 ? (
+                      <tr>
+                        <th
+                          className="uppercase tracking-wide font-medium pt-[8rem] text-lg"
+                          colSpan={5}
+                        >
+                          No Consulting Slots Found!
+                        </th>
+                      </tr>
+                    ) : (
+                      getConsultingTime.map((val, index) => {
+                        return (
+                          <tr key={val.id}>
+                            <td className="py-2 px-4 border-b border-b-gray-50">
+                              <div className="flex items-center">
+                                {index + 1}
+                              </div>
+                            </td>
+                            <td className="py-3 px-4 border-b border-b-gray-50">
+                              <TdComponent things={val.details} />
+                            </td>
+                          </tr>
+                        );
+                      })
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            {/* machine time table */}
+            <div className="flex w-full flex-col items-center p-1 h-1/2">
+              <div className="text-2xl font-semibold tracking-wide">
+                Machine Time Slot
+              </div>
+              <div className="animate-fade-left animate-delay-75 w-full shadow-gray-400 shadow-inner border rounded-md border-gray-100 animate-once animate-ease-out overflow-auto h-[93%]">
+                <table className="w-full min-w-[460px] z-0">
+                  <thead className="uppercas ">
+                    <tr className="bg-[#1F2937] text-white rounded-md">
+                      <ThComponent
+                        moreClasses={"rounded-tl-md rounded-bl-md"}
+                        name="No."
+                      />
+                      <ThComponent name="Doctor Name" />
+                      <ThComponent name="Machine Name" />
+                      <ThComponent name="Slot" />
+                      <ThComponent
+                        name="Time"
+                        moreClasses={"rounded-tr-md rounded-br-md"}
+                      />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {getMachineTime.length === 0 ? (
+                      <tr>
+                        <th
+                          className="uppercase tracking-wide font-medium pt-[8rem] text-lg"
+                          colSpan={8}
+                        >
+                          No Machine Slots Found!
+                        </th>
+                      </tr>
+                    ) : (
+                      getMachineTime.map((val, index) => {
+                        return (
+                          <tr key={val.id}>
+                            <td className="py-2 px-4 border-b border-b-gray-50">
+                              <div className="flex items-center">
+                                {index + 1}
+                              </div>
+                            </td>
+                            <td className="py-3 px-4 border-b border-b-gray-50">
+                              <TdComponent things={val.details} />
+                            </td>
+                          </tr>
+                        );
+                      })
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
