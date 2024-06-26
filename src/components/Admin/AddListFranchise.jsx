f5a5a9ec0bbdd21bc330b22bf454ca0fb6b6ecfe
Vedansh111 <123576150+Vedansh111@users.noreply.github.com>
2024-06-26 17:52:59 +0530

    Added franchise

--- src/components/Admin/AddListFranchise.jsx
+++ src/components/Admin/AddListFranchise.jsx
@@ -9,6 +9,8 @@ import {
   Modal,
   ModalClose,
   ModalDialog,
+  Option,
+  Select,
   Stack,
 } from "@mui/joy";
 import React, { useState } from "react";
@@ -19,8 +21,8 @@ function AddListFranchise(props) {
   const { register, handleSubmit, reset } = useForm();
 
   const submittedData = (d) => {
-    console.log(d);
-    props.handleApi();
+    // console.log(d);
+    props.handleApi(d);
     reset();
   };
 
@@ -55,7 +57,7 @@ function AddListFranchise(props) {
                 <FormControl>
                   <FormLabel>{props.first_name} :-</FormLabel>
                   <Input
-                    placeholder="Name..."
+                    placeholder="First Name..."
                     name={`first_name`}
                     {...register(`first_name`)}
                     autoFocus
@@ -65,12 +67,132 @@ function AddListFranchise(props) {
                 <FormControl>
                   <FormLabel>{props.last_name} :-</FormLabel>
                   <Input
-                    placeholder="Name..."
+                    placeholder="Last Name..."
                     name={`last_name`}
                     {...register(`last_name`)}
                     autoFocus
                     required
                   />
+                </FormControl>
+              </Box>
+              <Box className="flex space-x-4">
+                <FormControl>
+                  <FormLabel>{props.email} :-</FormLabel>
+                  <Input
+                    type="email"
+                    placeholder="Email..."
+                    name={`email`}
+                    {...register(`email`)}
+                    autoFocus
+                    required
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>{props.password} :-</FormLabel>
+                  <Input
+                    type="password"
+                    placeholder="Password..."
+                    name={`password`}
+                    {...register(`password`)}
+                    autoFocus
+                    required
+                  />
+                </FormControl>
+              </Box>
+              <Box className="flex space-x-4">
+                <FormControl>
+                  <FormLabel>{props.mobile} :-</FormLabel>
+                  <Input
+                    type="number"
+                    placeholder="Mobile..."
+                    name={`mobile`}
+                    {...register(`mobile`)}
+                    autoFocus
+                    required
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>{props.city} :-</FormLabel>
+                  <Input
+                    placeholder="City..."
+                    name={`city`}
+                    {...register(`city`)}
+                    autoFocus
+                    required
+                  />
+                </FormControl>
+              </Box>
+              <Box className="flex space-x-4">
+                <FormControl>
+                  <FormLabel>{props.state} :-</FormLabel>
+                  <Input
+                    placeholder="State..."
+                    name={`state`}
+                    {...register(`state`)}
+                    autoFocus
+                    required
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>{props.pincode} :-</FormLabel>
+                  <Input
+                    type="number"
+                    placeholder="Pincode..."
+                    name={`pincode`}
+                    {...register(`pincode`)}
+                    autoFocus
+                    required
+                  />
+                </FormControl>
+              </Box>
+              <Box className="flex space-x-4">
+                <FormControl>
+                  <FormLabel>{props.amount} :-</FormLabel>
+                  <Input
+                    type="number"
+                    placeholder="Amount..."
+                    name={`amount`}
+                    {...register(`amount`)}
+                    autoFocus
+                    required
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>{props.commission} :-</FormLabel>
+                  <Input
+                    type="number"
+                    placeholder="In %..."
+                    name={`commission`}
+                    {...register(`commission`)}
+                    autoFocus
+                    required
+                  />
+                </FormControl>
+              </Box>
+              <Box className="flex w-full space-x-4">
+                <FormControl className="w-1/2">
+                  <FormLabel>{props.type_of_admin} :-</FormLabel>
+                  <Select
+                    required
+                    placeholder="Admin Type..."
+                    name={`type_of_admin`}
+                    {...register(`type_of_admin`)}
+                  >
+                    <Option value="super_admin">Super Admin</Option>
+                    <Option value="franchise">Franchise</Option>
+                  </Select>
+                </FormControl>
+                <FormControl className="w-1/2">
+                  <FormLabel>{props.possibility_group} :-</FormLabel>
+                  <Select
+                    required
+                    placeholder="Possibility Group..."
+                    name={`possibility_group`}
+                    {...register(`possibility_group`)}
+                  >
+                    <Option value="yes">Yes</Option>
+                    <Option value="no">No</Option>
+                  </Select>
                 </FormControl>
               </Box>
               <Button type="submit">Submit</Button>
--- src/pages/Admin/customer/NewCustomer.jsx
+++ src/pages/Admin/customer/NewCustomer.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import Stepper from '@mui/joy/Stepper';
-import Step from '@mui/joy/Step';
-import StepButton from '@mui/joy/StepButton';
-import StepIndicator from '@mui/joy/StepIndicator';
-import Check from '@mui/icons-material/Check';
+import { useState } from "react";
+import Stepper from "@mui/joy/Stepper";
+import Step from "@mui/joy/Step";
+import StepButton from "@mui/joy/StepButton";
+import StepIndicator from "@mui/joy/StepIndicator";
+import Check from "@mui/icons-material/Check";
 import CustomerGeneralDetails from "./new_customer/CustomerGeneralDetails";
 import QueCurrentDiet from "../../User/Questions/QueCurrentDiet";
-import QueFamilyHistory from '../../User/Questions/QueFamilyHistory';
-import QueComplains from '../../User/Questions/QueComplains';
-import CustomerQuestionsPart1 from './new_customer/CustomerQuestionsPart1';
-import CustomerQuestionsPart2 from './new_customer/CustomerQuestionsPart2';
-import QueCheckout from '../../User/Questions/QueCheckout';
+import QueFamilyHistory from "../../User/Questions/QueFamilyHistory";
+import QueComplains from "../../User/Questions/QueComplains";
+import CustomerQuestionsPart1 from "./new_customer/CustomerQuestionsPart1";
+import CustomerQuestionsPart2 from "./new_customer/CustomerQuestionsPart2";
+import QueCheckout from "../../User/Questions/QueCheckout";
 const steps = [
-  'General Details',
-  'Current Diet',
-  'Family History',
-  'Complains',
-  'Questions',
-  'Diagnosis',
-  'Checkout'
+  "General Details",
+  "Current Diet",
+  "Family History",
+  "Complains",
+  "Questions",
+  "Diagnosis",
+  "Checkout",
 ];
 function NewCustomer() {
   const [currentStep, setCurrentStep] = useState(0);
@@ -41,24 +41,25 @@ function NewCustomer() {
         .
       </div>
       <div className=" h-screen flex-grow overflow-auto flex flex-wrap content-start p-3">
-      <Stepper sx={{ width: '100%', height:"7%"}}>
+        <Stepper sx={{ width: "100%", height: "7%" }}>
           {steps.map((step, index) => (
             <Step
               key={step}
               orientation="vertical"
               indicator={
                 <StepIndicator
-                  variant={currentStep <= index ? 'outlined' : 'solid'}
-                  color={currentStep < index ? 'success' : 'success'}
+                  variant={currentStep <= index ? "outlined" : "solid"}
+                  color={currentStep < index ? "success" : "success"}
                 >
                   {currentStep <= index ? index + 1 : <Check />}
                 </StepIndicator>
               }
               sx={{
-                '&::after': {
-                  ...(currentStep > index && index !== steps.length - 1 && {
-                    bgcolor: 'success.solidBg',
-                  }),
+                "&::after": {
+                  ...(currentStep > index &&
+                    index !== steps.length - 1 && {
+                      bgcolor: "success.solidBg",
+                    }),
                 },
               }}
             >
@@ -68,13 +69,31 @@ function NewCustomer() {
             </Step>
           ))}
         </Stepper>
-        {currentStep === 0 && <CustomerGeneralDetails onNext={handleNextStep} />}
-        {currentStep === 1 && <QueCurrentDiet onNext={handleNextStep} onBack={handleBackStep} />}
-        {currentStep === 2 && <QueFamilyHistory onNext={handleNextStep} onBack={handleBackStep} />}
-        {currentStep === 3 && <QueComplains onNext={handleNextStep} onBack={handleBackStep} />}
-        {currentStep === 4 && <CustomerQuestionsPart1 onNext={handleNextStep} onBack={handleBackStep} />}
-        {currentStep === 5 && <CustomerQuestionsPart2 onNext={handleNextStep} onBack={handleBackStep} />}
-        {currentStep === 6 && <QueCheckout  onBack={handleBackStep} />}
+        {currentStep === 0 && (
+          <CustomerGeneralDetails onNext={handleNextStep} />
+        )}
+        {currentStep === 1 && (
+          <QueCurrentDiet onNext={handleNextStep} onBack={handleBackStep} />
+        )}
+        {currentStep === 2 && (
+          <QueFamilyHistory onNext={handleNextStep} onBack={handleBackStep} />
+        )}
+        {currentStep === 3 && (
+          <QueComplains onNext={handleNextStep} onBack={handleBackStep} />
+        )}
+        {currentStep === 4 && (
+          <CustomerQuestionsPart1
+            onNext={handleNextStep}
+            onBack={handleBackStep}
+          />
+        )}
+        {currentStep === 5 && (
+          <CustomerQuestionsPart2
+            onNext={handleNextStep}
+            onBack={handleBackStep}
+          />
+        )}
+        {currentStep === 6 && <QueCheckout onBack={handleBackStep} />}
       </div>
     </div>
   );
