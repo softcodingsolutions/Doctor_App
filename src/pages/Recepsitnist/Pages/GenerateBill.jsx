1c5d2c99703f194f31cf87019d0148eba1a03c58
Richa Modi <123286601+RichiErichhH@users.noreply.github.com>
2024-07-06 14:02:40 +0530

    6th_July

--- src/pages/Recepsitnist/Pages/GenerateBill.jsx
+++ src/pages/Recepsitnist/Pages/GenerateBill.jsx
@@ -5,12 +5,15 @@ import { useDebounce } from "use-debounce";
 export default function GenerateBill() {
   const [searchTerm, setSearchTerm] = useState("");
   const [debouncedSearchTerm] = useDebounce(searchTerm, 500);
-  const [id,setId] = useState(0);
-  const [userDetails,setUser] = useState({});
+  const [id, setId] = useState(0);
+  const [userDetails, setUser] = useState({});
+  const [medicines, setMedicines] = useState([]);
+  const [treatment, setTreatment] = useState([]);
+
   const handleSearchTerm = (value) => {
     setSearchTerm(value);
   };
-  
+
   useEffect(() => {
     if (debouncedSearchTerm) {
       axios
@@ -37,38 +40,70 @@ export default function GenerateBill() {
         });
     }
   }, [debouncedSearchTerm]);
-  useEffect(() =>{
-    axios.get(`/api/v2/users/search?id=${id}`).then((res)=>{
-      console.log(res,"USER PACKAGE");
-      setUser(res.data.user);
-    }).catch((err)=>{
-      console.log(err)
-    })
-  },[id])
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`/api/v2/users/search?id=${id}`)
+        .then((res) => {
+          console.log(res, "USER PACKAGE");
+          setUser(res.data.user);
+          setMedicines(
+            res.data.user.treatment_packages.flatMap((data) =>
+              data.treatment_package.medicines
+            )
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [id]);
+
   return (
     <div className="w-full p-5">
-      <div className="rounded-lg bg-card h-[90vh] bg-white">
+      <div className="rounded-lg bg-white h-[90vh] shadow-lg">
         <div className="flex flex-col px-4 py-3 h-full space-y-4">
           <div>
-            <div className="text-xl font-semibold">Generate Bill</div>
-            <div className="flex gap-5 p-2 w-full">
+            <div className="text-2xl font-semibold mb-4">Generate Bill</div>
+            <div className="flex gap-5 p-2 w-full mb-4">
               <input
-                    type="text"
-                    value={searchTerm}
-                    onChange={(e) => handleSearchTerm(e.target.value)}
-                    placeholder="search case number"
-                    className="py-1 px-2 rounded-md border border-black w-full"
-                />  
+                type="text"
+                value={searchTerm}
+                onChange={(e) => handleSearchTerm(e.target.value)}
+                placeholder="Search case number or phone number"
+                className="py-2 px-4 rounded-md border border-gray-300 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
             </div>
-            <div className="w-full flex justify-center  shadow-gray-400 shadow-inner border rounded-md border-gray-100 animate-once animate-ease-out overflow-auto h-[93%]">
-              <div className="flex flex-col text-lg font-bold justify-center w-4/5 ">
-                <div className="flex justify-around ">
-                  <div>
-                    Patient Name:{" "}
+            <div className="w-full flex-col p-5 shadow-gray-400 shadow-inner border rounded-md border-gray-200 overflow-auto h-[75vh]">
+              <div className="flex flex-col text-lg font-bold mb-6">
+                <div>
+                  Patient Name:{" "}
+                  <span className="font-medium">
                     {userDetails.first_name} {userDetails.last_name}
-                  </div>
-                  <div>Case Number: {userDetails.case_number}</div>
+                  </span>
                 </div>
+                <div>
+                  Case Number:{" "}
+                  <span className="font-medium">{userDetails.case_number}</span>
+                </div>
+              </div>
+              <div className="text-md font-semibold">
+                {medicines.map((med, index) => (
+                  <div
+                    key={index}
+                    className="flex justify-between items-center mb-4 p-5 border rounded-md border-gray-200"
+                  >
+                    <div className="flex flex-col">
+                      <span className="font-medium">Medicine Name:</span>
+                      <span>{med.medicine_name}</span>
+                    </div>
+                    <div className="flex flex-col mr-20">
+                      <span className="font-medium">Quantity:</span>
+                      <span>{med.medicine_quantity}</span>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
