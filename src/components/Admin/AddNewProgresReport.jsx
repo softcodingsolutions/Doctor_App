13fc5a9ec650ed8aaa49e86c047887adc8160c36
Vedansh111 <123576150+Vedansh111@users.noreply.github.com>
2024-07-15 17:44:45 +0530

    changes on the report page

--- src/components/Admin/AddNewProgresReport.jsx
+++ src/components/Admin/AddNewProgresReport.jsx
@@ -19,44 +19,12 @@ import axios from "axios";
 
 function AddNewProgresReport(props) {
   const [open, setOpen] = useState(false);
-  const [getWeight, setGetWeight] = useState([]);
-  const [getPackage, setGetPackage] = useState([]);
   const { register, handleSubmit, reset } = useForm();
-
-  const handleGetWeight = () => {
-    axios
-      .get("/api/v1/weight_reasons")
-      .then((res) => {
-        console.log(res.data);
-        setGetWeight(res.data?.weight_reasons);
-      })
-      .catch((err) => {
-        console.log(err);
-        alert(err.message);
-      });
-  };
-
-  const handleGetPackages = async (e) => {
-    await axios
-      .get(`/api/v1/treatment_packages?weight_reason=${e.target.innerHTML}`)
-      .then((res) => {
-        console.log(res.data?.treatment_packages);
-        setGetPackage(res.data?.treatment_packages);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   const submittedData = (d) => {
     console.log(d);
-    props.handleApi(d.package_id);
+    props.handleApi(d.progress_date, d.progress_weight);
     reset();
   };
-
-  useEffect(() => {
-    handleGetWeight();
-  }, []);
 
   return (
     <React.Fragment>
@@ -106,58 +74,6 @@ function AddNewProgresReport(props) {
                   required
                 />
               </FormControl>
-
-              <Box className="flex space-x-4 items-center">
-                <FormControl>
-                  <FormLabel>{props.weight_reason} :-</FormLabel>
-                  <Select
-                    className="w-full"
-                    required
-                    placeholder="Choose any one..."
-                    name={`weight_reason`}
-                    {...register(`weight_reason`)}
-                  >
-                    {getWeight.map((res) => {
-                      return (
-                        <Option
-                          key={res.id}
-                          onClick={handleGetPackages}
-                          value={res.name}
-                        >
-                          {res.name}
-                        </Option>
-                      );
-                    })}
-                  </Select>
-                </FormControl>
-
-                {getPackage.length > 0 && (
-                  <FormControl>
-                    <FormLabel>Select Packages:-</FormLabel>
-                    <Select
-                      className="w-full"
-                      required
-                      placeholder="Choose any one..."
-                      name={`package_id`}
-                      {...register(`package_id`)}
-                    >
-                      {getPackage?.map((res) => {
-                        return (
-                          <Option key={res.id} value={res.id}>
-                            {res.package_name}
-                          </Option>
-                        );
-                      })}
-                    </Select>
-                  </FormControl>
-                )}
-
-                {getPackage.length === 0 && (
-                  <FormControl>
-                    <FormLabel>No Packages Found!</FormLabel>
-                  </FormControl>
-                )}
-              </Box>
 
               <Button type="submit">Submit</Button>
             </Stack>
