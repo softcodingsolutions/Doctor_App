414f5fa4de9afe3665fc92f4082da881b366a1ff
Richa Modi <123286601+RichiErichhH@users.noreply.github.com>
2024-07-27 19:48:34 +0530

    Survey

---
+++ src/components/Admin/AddHealthProblem.jsx
@@ -0,0 +1,125 @@
+import { Add } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  DialogTitle,
+  FormControl,
+  FormLabel,
+  Modal,
+  ModalClose,
+  ModalDialog,
+  Stack,
+} from "@mui/joy";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { ReactTransliterate } from "react-transliterate";
+
+function AddHealthProblem(props) {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState({
+    gujarati: "",
+    english: "",
+  });
+  const { register, handleSubmit, reset } = useForm();
+
+  const submittedData = (d) => {
+    console.log(text);
+    console.log(d);
+    props.handleApi(text.gujarati, text.english);
+    reset();
+    setText({
+      gujarati: "",
+      english: "",
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <Button
+        variant="outlined"
+        color="neutral"
+        startDecorator={<Add />}
+        onClick={() => setOpen(true)}
+      >
+        {props.name}
+      </Button>
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        <ModalDialog>
+          <ModalClose />
+          <DialogTitle>{props.title}</DialogTitle>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              handleSubmit(submittedData)(event);
+              setOpen(false);
+            }}
+          >
+            <Stack spacing={3}>
+              <FormControl>
+                <FormLabel>{props.details} :-</FormLabel>
+                <Box className="flex flex-col items-center w-full">
+                  <ReactTransliterate
+                    name={`question_english`}
+                    {...register(`question_english`)}
+                    value={text.english}
+                    lang="en"
+                    onChangeText={(value) => {
+                      setText((prev) => {
+                        return {
+                          ...prev,
+                          english: value,
+                        };
+                      });
+                    }}
+                    renderComponent={(props) => {
+                      return (
+                        <textarea {...props} placeholder="In English..." />
+                      );
+                    }}
+                    className="p-1 border border-gray-400 rounded-sm"
+                    rows={4}
+                    cols={30}
+                    required
+                  />
+
+                  <ReactTransliterate
+                    name={`question_gujarati`}
+                    {...register(`question_gujarati`)}
+                    value={text.gujarati}
+                    lang="gu"
+                    onChangeText={(value) => {
+                      setText((prev) => {
+                        return {
+                          ...prev,
+                          gujarati: value,
+                        };
+                      });
+                    }}
+                    renderComponent={(props) => {
+                      return (
+                        <textarea {...props} placeholder="In Gujarati..." />
+                      );
+                    }}
+                    className="p-1 border border-gray-400 rounded-sm"
+                    rows={4}
+                    cols={30}
+                    required
+                  />
+                </Box>
+              </FormControl>
+
+              <Button type="submit">Submit</Button>
+            </Stack>
+          </form>
+        </ModalDialog>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+export default AddHealthProblem;
--- src/pages/Admin/surveymaster/Weightgainreason.jsx
+++ src/pages/Admin/surveymaster/Weightgainreason.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
-
+import { MdDelete } from "react-icons/md";
+import TdComponent from "../../../components/TdComponent";
+import ThComponent from "../../../components/ThComponent";
+import axios from "axios";
+import Swal from "sweetalert2";
+import AddWeightGain from '../../../components/Admin/AddWeightGain';
 export default function Weightgainreason() {
+
+  const handleWeightQuestion = (gujarati, english) => {
+    
+  };
+
+
   return (
-    <div>
-      <h1>Weight Gain Reason</h1>
+    <div className="w-full p-2">
+        <div className="rounded-lg bg-card h-[85vh] bg-white">
+            <div className="flex px-4 py-3 h-full flex-col space-y-4">
+                <div className="flex items-center justify-between">
+                  <div className="font-semibold text-xl">Weight Gain Reason</div>
+                  <AddWeightGain
+                    handleApi={handleWeightQuestion}
+                    name="Add Weight Gain Reason"
+                    title="Add New Question"
+                    details="Details"
+                  />
+                </div>
+
+                <div className="animate-fade-left animate-delay-75 shadow-gray-400 shadow-inner border rounded-md border-gray-100 animate-once animate-ease-out overflow-auto h-[93%]">
+                  <table className="w-full min-w-[460px] z-0">
+                    <thead className="uppercase ">
+                        <tr className="bg-[#1F2937] text-white rounded-md">
+                          <ThComponent
+                            moreClasses={"rounded-tl-md rounded-bl-md"}
+                            name="No."
+                          />
+                          <ThComponent name="In English" />
+                          <ThComponent name="In Gujarati" />
+                          <ThComponent />
+                          <ThComponent moreClasses={"rounded-tr-md rounded-br-md"} />
+                        </tr>
+                    </thead>
+                  </table>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
