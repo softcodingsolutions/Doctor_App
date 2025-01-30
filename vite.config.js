0741ed31db2161d5823b91307da1fc4c06051ea9
Richa Modi <123286601+RichiErichhH@users.noreply.github.com>
2025-01-30 17:00:48 +0530

    config vit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
-    port: 5252, 
+    watch: {
+      followSymlinks: false,
+    },
+    port: 5252,
     open: true,
   },
   build: {
-    outDir: "dist", 
+    outDir: "dist",
   },
   plugins: [react()],
 });
