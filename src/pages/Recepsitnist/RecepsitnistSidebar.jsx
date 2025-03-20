f90144cef13b9bd80d26a3935e3d32d0710ee8ae
Richa Modi <123286601+RichiErichhH@users.noreply.github.com>
2025-03-20 15:11:54 +0530

    change the icon for consulting appointment

--- src/pages/Recepsitnist/RecepsitnistSidebar.jsx
+++ src/pages/Recepsitnist/RecepsitnistSidebar.jsx
@@ -11,7 +11,7 @@ import { IoCloseOutline, IoPersonSharp } from "react-icons/io5";
 import { IoIosSend } from "react-icons/io";
 import { GrVirtualMachine } from "react-icons/gr";
 import icons_slime from "../../assets/images/icons_slime_converted.webp";
-import { SiPowerapps } from "react-icons/si";
+import { FaFilePen } from "react-icons/fa6";
 
 function RecepsitnistSidebar({ onSidebarHide, showSidebar, admin }) {
   const navigate = useNavigate();
@@ -37,7 +37,7 @@ function RecepsitnistSidebar({ onSidebarHide, showSidebar, admin }) {
       id: "2",
       title: "Consulting Appointment",
       to: "appointment/create-appointment",
-      icons: <SiPowerapps size={18} />,
+      icons: <FaFilePen size={18} />,
     },
     {
       id: "7",
